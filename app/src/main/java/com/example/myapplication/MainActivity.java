fbb96c06c5f810621e16aff0012a2a7474f33166
rinyang2 <49273615+rinyang2@users.noreply.github.com>
2022-05-26 15:58:14 +0300

    TextView and Button practice: Sum number

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button addBtn = (Button) findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                EditText firstNumEditText = (EditText) findViewById(R.id.editTextTextPersonName);
+                EditText secondNumEditText = (EditText) findViewById(R.id.editTextTextPersonName2);
+                TextView resultTextView = (TextView) findViewById(R.id.resultText);
+
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                int result = num1+num2;
+                resultTextView.setText(result+"");
+            }
+        });
         //comment
     }
 }
